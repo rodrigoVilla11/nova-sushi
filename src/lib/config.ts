cce206439ee28f88c017f55036720c8cf879458d
Rodrigo PC <villarrealrodrigo.n@gmail.com>
2026-08-09 09:55:07 +0200

    Cambio de numero

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const CONTACT = {
-  whatsapp: "5493512583838",
+  whatsapp: "5493516827638",
   whatsappAdmin: "5493547570006",
   email: "info@nova-sushi.com",
   phone: "+54 9 351 258 3838",
@@ -14,8 +14,8 @@ export const BRANCHES = [
     name: "Nueva Córdoba",
     address: "Ambrosio Olmos 939, Córdoba",
     phone: "+54 9 351 258 3838",
-    tel: "5493512583838",
-    whatsapp: "5493512583838",
+    tel: "5493516827638",
+    whatsapp: "5493516827638",
     mapsUrl: "https://maps.app.goo.gl/hhF6HzHDkayGUz338",
     mapsEmbed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3404.3513947933784!2d-64.19434892468284!3d-31.431991697005405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9432a3375f3deab1%3A0x96540e6d01fdbe4d!2sN%C3%95VA%20SUSHI%20NUEVA!5e0!3m2!1ses!2sar!4v1754881681784!5m2!1ses!2sar",
   },
